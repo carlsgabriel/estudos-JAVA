b46447251b58eea69d4e7cfd28ec7185c5d087f1
carlsgabriel <gomescarlosleite@gmail.com>
2025-05-31 14:17:47 -0300

    sintaxe exercises finished

---
+++ Exercicios - Fuctura/Módulo 1/Exercicíos recomendados/Sintaxe/src/Principal.java
@@ -0,0 +1,66 @@
+public class Principal {
+
+    public static void main(String[] args){
+        
+        // Utilizando System.out.println(), imprima seu sobrenome.
+        System.out.println("Exercicio 01:");
+        System.out.println("Gabriel");
+        System.out.println();
+
+        // Com `System.out.println()`, exiba o resultado da multiplicação de 3 por 9.
+        System.out.println("Exercicio 02:");
+        System.out.println(3 * 9);
+        System.out.println();
+
+        // Imprima uma frase curta sobre o clima atual usando `System.out.println()`.
+        System.out.println("Exercicio 03:");
+        System.out.println("Está chovendo muito ultimamente.");
+        System.out.println();
+
+        // Mostre a hora atual usando `System.out.println()`.
+        System.out.println("Exercicio 04:");
+        System.out.println("São exatamente 10 horas, 55 minutos e 23 segundos.");
+        System.out.println();
+
+        // Utilizando `System.out.print()`, imprima seu nome.
+        System.out.println("Exercicio 05:");
+        System.out.print("Carlos Gabriel\n");
+        System.out.println();
+
+        // Com `System.out.print()`, exiba o resultado da soma de 5 + 7.
+        System.out.println("Exercicio 06:");
+        System.out.print(5 + 7 + "\n");
+        System.out.println();
+
+        // Imprima uma mensagem com três palavras de sua escolha usando System.out.print()
+        System.out.println("Exercicio 07:");
+        System.out.print("Quero ser programador\n");
+        System.out.println();
+
+        // Mostre a data de hoje (ou qualquer data que você preferir) usando System.out.print()
+        System.out.println("Exercicio 08:");
+        System.out.print("27/05/2025\n");
+        System.out.println();
+
+        // Utilizando `System.out.printf(),` imprima seu nome e idade no formato "Nome:[seu nome], Idade: [sua idade]".
+        System.out.println("Exercicio 09:");
+        System.out.printf("Nome: %s, Idade: %d%n", "Carlos Gabriel", 25);
+        System.out.println();
+
+        // Com `System.out.printf(),`, exiba o resultado da divisão de 20 por 4 no formato "Resultado: [resultado]".
+        System.out.println("Exercicio 10:");
+        System.out.printf("Resultado: %d%n", 20 * 4);
+        System.out.println();
+
+        // Imprima um número decimal com duas casas decimais usando System.out.printf()
+        System.out.println("Exercicio 11:");
+        System.out.printf("Resultado: %.2f%n", 23.489);
+        System.out.println();
+
+        // Mostre seu peso com uma precisão de uma casa decimal usando System.out.printf()
+        System.out.println("Exercicio 12:");
+        System.out.printf("Resultado: %.1f", 67.00);
+
+    }
+
+}
