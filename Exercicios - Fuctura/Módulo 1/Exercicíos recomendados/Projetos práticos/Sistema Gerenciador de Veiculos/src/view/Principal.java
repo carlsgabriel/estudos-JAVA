2b38a9bb6972856b20d38cd7cedd5fbcff0d8995
carlsgabriel <gomescarlosleite@gmail.com>
2025-07-01 22:51:17 -0300

    adicionando class Principal porque é necessário e para testar o projeto

---
+++ Exercicios - Fuctura/Módulo 1/Exercicíos recomendados/Projetos práticos/Sistema Gerenciador de Veiculos/src/view/Principal.java
@@ -0,0 +1,15 @@
+package view;
+
+import controller.Menu;
+
+public class Principal {
+
+    public static void main(String[] args){
+
+        Menu menu = new Menu();
+
+        menu.exibirMenu();
+
+    }
+
+}
